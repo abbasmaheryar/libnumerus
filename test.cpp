07e4fe8e8f7d3e01c8b1cc0736eaf0edec0a1f92
abbas <abbasmaheryar@gmail.com>
2020-08-19 12:50:54 -0700

    exit failure for wrong # of arguments

--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
 #include <cstdlib>
-#include <iostream>
 #include <vector>
-
+#include <iostream>
 #include "bigNum.h"
 
 int main(int argc, char* argv[]) {
+	if (argc == 1 || argc > 2) {
+		std::cout << "Please enter the bit-length for the random number you want to generate!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 	int size = atoi(argv[1]);
 	BigNum num(size);
 	num.generateRand();
